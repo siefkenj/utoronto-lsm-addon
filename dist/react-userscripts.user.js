c50b554ecf1e4f005943dd1c98b0a4c6492468be
Jason Siefken <siefkenj@gmail.com>
2026-08-27 15:37:32 +0000

    Update version

--- dist/react-userscripts.user.js
+++ dist/react-userscripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     utoronto LSM Addon
 // @namespace https://github.com/siefkenj/utoronto-lsm-addons
-// @version  2.0.0
+// @version  2.0.1
 // @description A sample userscript built using react
 // @include https://lsm.utoronto.ca/*
 // @include https://ttb.utoronto.ca/*
